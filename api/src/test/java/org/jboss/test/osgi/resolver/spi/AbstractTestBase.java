032b64d5f5ca748323033fb1254ea4caadb95e89
Thomas Diesler <thomas.diesler@jboss.com>
2012-10-18 13:06:48 +0200

    Update spi to fix invalid transistive dependency to jbosgi-metadata

--- api/src/test/java/org/jboss/test/osgi/resolver/spi/AbstractTestBase.java
+++ api/src/test/java/org/jboss/test/osgi/resolver/spi/AbstractTestBase.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.jar.Attributes.Name;
 
 import org.jboss.osgi.metadata.OSGiMetaData;
-import org.jboss.osgi.metadata.internal.AbstractOSGiMetaData;
+import org.jboss.osgi.metadata.spi.AbstractOSGiMetaData;
 import org.jboss.osgi.resolver.XEnvironment;
 import org.jboss.osgi.resolver.XResource;
 import org.jboss.osgi.resolver.XResourceBuilder;
